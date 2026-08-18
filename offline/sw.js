fb95ad8d6acc3040464badf10a65e7adea5def88
hasiniande <hasiniande09@gmail.com>
2026-08-18 23:43:37 +0530

    Update web manifests and HTML head tags to use custom citizen and rescue app icons

--- offline/sw.js
+++ offline/sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'aegis-offline-v3';
+const CACHE_NAME = 'aegis-offline-v4';
 const ASSETS_TO_CACHE = [
   './citizen-app.html',
   './rescue-app.html',
   './offline-styles.css',
   './citizen.png',
   './rescue.png',
+  './citizen-icon.png',
+  './rescue-icon.png',
   './manifest-citizen.json',
   './manifest-rescue.json'
 ];
