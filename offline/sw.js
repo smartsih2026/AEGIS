685898a4f41bd16e0eb12865850689249a6dc9ec
hasiniande <hasiniande09@gmail.com>
2026-08-18 23:43:33 +0530

    Add AEGIS Mobile Offline Citizen & Rescue PWA apps

---
+++ offline/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'aegis-offline-v1';
+const ASSETS_TO_CACHE = [
+  './citizen-app.html',
+  './rescue-app.html',
+  './offline-styles.css',
+  './manifest-citizen.json',
+  './manifest-rescue.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
